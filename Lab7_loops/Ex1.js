c66b3b1a143e66945b305f3307b5c667b4cf2762
JoseDavidLP <jdlpang@gmail.com>
2022-02-22 13:25:27 -1000

    lab7_loops finish

--- Lab7_loops/Ex1.js
+++ Lab7_loops/Ex1.js
@@ -7,8 +7,10 @@ while(count <= num_products) {
 
     console.log(`${count}. ${eval('name' + count)}`);
     count++;
-    if (count > num_products/2) {
-        break;
+    if(count > 0.25*num_products && count<0.75*num_products){
+        console.log(`Item #${count} is sold out!`);
+        continue;
     }
 }
 console.log("That's all we have!")
+
